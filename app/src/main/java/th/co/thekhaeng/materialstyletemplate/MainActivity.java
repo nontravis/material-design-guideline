26248788d563e00cc16c8c0cee052859e1d02593
TheKhaeng <nonthawit.kub@gmail.com>
2017-04-05 15:26:36 +0700

    [+] first commit

---
+++ app/src/main/java/th/co/thekhaeng/materialstyletemplate/MainActivity.java
@@ -0,0 +1,14 @@
+package th.co.thekhaeng.materialstyletemplate;
+
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState ){
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
+    }
+}
